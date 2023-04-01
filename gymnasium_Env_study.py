bf45562f7f7d04fa874e4319d6d15b2eed534ab7
wanggc <1805504238@qq.com>
2023-04-01 22:06:47 +0800

    优化代码

--- gymnasium_Env_study.py
+++ gymnasium_Env_study.py
@@ -1,39 +1,44 @@
 import gymnasium as gym
 
-# 创建一个"LunarLander-v2"环境
-env = gym.make("LunarLander-v2", render_mode="human")
+class LunarLanderSimulator:
+    def __init__(self, seed=42):
+        self.env = gym.make("LunarLander-v2", render_mode="human")
+        self.env.action_space.seed(seed)
+        self.seed = seed
 
-# 了解Env类的核心方法和属性
-print("Action space:", env.action_space)
-print("Observation space:", env.observation_space)
-print("Reward range:", env.reward_range)
-print("Environment spec:", env.spec)
-print("Environment metadata:", env.metadata)
+    def print_env_info(self):
+        print("Action space:", self.env.action_space)
+        print("Observation space:", self.env.observation_space)
+        print("Reward range:", self.env.reward_range)
+        print("Environment spec:", self.env.spec)
+        print("Environment metadata:", self.env.metadata)
 
-# 设置随机数生成器种子
-env.action_space.seed(123)
+    def reset(self):
+        observation, info = self.env.reset(seed=self.seed)
+        return observation, info
 
-# 重置环境
-observation, info = env.reset(seed=42)
+    def run_simulation(self, num_steps=1000):
+        observation, info = self.reset()
 
-# 循环执行1000步操作
-for _ in range(1000):
-    # 随机选择动作（在这里插入你的策略）
-    action = env.action_space.sample()
+        for _ in range(num_steps):
+            action = self.env.action_space.sample()
+            observation, reward, terminated, truncated, info = self.env.step(action)
 
-    # 执行动作，获取新的观察、奖励和环境状态
-    observation, reward, terminated, truncated, info = env.step(action)
+            print(f"Observation: {observation}")
+            print(f"Reward: {reward}")
+            print(f"Terminated: {terminated}")
+            print(f"Truncated: {truncated}")
+            print(f"Info: {info}")
 
-    # 打印step()方法返回的信息
-    print(f"Observation: {observation}")
-    print(f"Reward: {reward}")
-    print(f"Terminated: {terminated}")
-    print(f"Truncated: {truncated}")
-    print(f"Info: {info}")
+            if terminated or truncated:
+                observation, info = self.reset()
 
-    # 如果环境终止或截断，重置环境
-    if terminated or truncated:
-        observation, info = env.reset()
+    def close(self):
+        self.env.close()
 
-# 关闭环境
-env.close()
+
+if __name__ == "__main__":
+    simulator = LunarLanderSimulator()
+    simulator.print_env_info()
+    simulator.run_simulation()
+    simulator.close()
