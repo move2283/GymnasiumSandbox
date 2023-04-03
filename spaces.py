9792c75307bb5ff59781365baa347c6ed340d9e7
wanggc <1805504238@qq.com>
2023-04-03 11:12:05 +0800

    增加了一些 Discrete类型

--- spaces.py
+++ spaces.py
@@ -3,8 +3,9 @@ from gymnasium import spaces
 
 class MyEnvironment:
     def __init__(self):
-        self.action_space = spaces.Discrete(3)
+        self.action_space = spaces.Discrete(3, seed=42)
         self.observation_space = spaces.Box(0, 1, shape=(2,))
+        self.discrete_space = spaces.Discrete(3, start=-1, seed=42)
         self.box_space_1 = spaces.Box(low=-1.0, high=2.0, shape=(3, 4), dtype=np.float64)
         self.box_space_2 = spaces.Box(low=np.array([-1.0, -2.0]), high=np.array([2.0, 4.0]), dtype=np.float64)
 
@@ -13,7 +14,7 @@ def print_info(env, space_name):
     sample = space.sample()
     print(f"Sampled value from {space_name}: {sample}")
 
-    if space_name in ["action_space", "observation_space"]:
+    if space_name in ["action_space", "observation_space", "discrete_space"]:
         is_valid = space.contains(sample)
         print(f"Is {space_name[:-6]} valid? {is_valid}")
 
@@ -24,6 +25,6 @@ def print_info(env, space_name):
 if __name__ == "__main__":
     env = MyEnvironment()
 
-    for space_name in ["action_space", "observation_space", "box_space_1", "box_space_2"]:
+    for space_name in ["action_space", "observation_space", "discrete_space", "box_space_1", "box_space_2"]:
         print_info(env, space_name)
         print()
